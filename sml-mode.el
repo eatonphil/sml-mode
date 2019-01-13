d075769fb8ffc587f7fbc894052d5b1fc43bb6ca
Phil Eaton <philneaton95@gmail.com>
2019-01-13 17:59:08 -0500

    First commit

---
+++ sml-mode.el
@@ -0,0 +1,46 @@
+(defvar sml-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?\( ". 1" st)
+    (modify-syntax-entry ?* ". 23" st)
+    (modify-syntax-entry ?\) ". 4" st)
+    st))
+
+(defvar keywords
+  (regexp-opt
+   '("struct" "structure" "end" "local" "fun"
+     "val" "in" "let" "open" "include" "ref"
+     "case" "of")
+   'words))
+
+(defvar operators
+  (regexp-opt
+   '(":" "=" ":=" "!" "<>" "^" "+" "-" "*" "/" "::"
+     "=>" "->" ">" "<" "~" "|" "_" "," ".")
+   nil))
+
+(defvar builtins
+  (regexp-opt
+   '("list" "int" "bool" "double" "string" "char")
+   'symbols))
+
+(defvar comment-delimiters
+  (regexp-opt
+   '("(*" "*)")
+   'symbols))
+
+(defvar sample-font-lock-keywords
+  `((,keywords . font-lock-keyword-face)
+    (,comment-delimiters . font-lock-comment-delimiter-face)
+    (,operators . font-lock-comment-delimiter-face)
+    (,builtins . font-lock-builtin-face)
+    ("\\<fun \\([a-zA-Z][a-zA-Z0-9]+\\)\\>" 1 font-lock-function-name-face)))
+
+(define-derived-mode sml-mode fundamental-mode "Standard ML"
+  "A major mode for Standard ML."
+  :syntax-table sml-mode-syntax-table
+  (setq comment-multi-line nil)
+  (setq-local comment-start "(*")
+  (setq-local comment-end "*)")
+  (setq-local font-lock-defaults '(sample-font-lock-keywords)))
+
+(provide 'sml-mode)
